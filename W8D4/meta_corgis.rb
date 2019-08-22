a3edfec86e321fef16fdddce39cf3ca7e52c4632
kchhak <kane.chhak@gmail.com>
2019-08-22 07:00:51 -0700

    Create MetaCorgiSnacks#method_missing

--- W8D4/meta_corgis.rb
+++ W8D4/meta_corgis.rb
@@ -110,7 +110,12 @@ class MetaCorgiSnacks
   end
 
   def method_missing(name, *args)
-    # Your code goes here...
+    info = @snack_box.send("get_#{name}_info", @box_id)
+    tastiness = @snack_box.send("get_#{name}_tastiness", @box_id)
+    name = name.capitalize
+    result = "#{name}: #{info}: #{tastiness}" 
+    tastiness > 30 ? "* #{result}" : result
+
   end
 
 
