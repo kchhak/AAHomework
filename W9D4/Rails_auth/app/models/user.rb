8b4d10042298a395477a96d79a840cf814e35f70
kchhak <kane.chhak@gmail.com>
2019-08-29 04:35:58 -0700

    Create Users table and User model validations

---
+++ W9D4/Rails_auth/app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ApplicationRecord
+  validates :username, :session_token, presence: true
+  validates :password_digest, presence: {message: "Password can't be blank"}
+  validates :password, length: {minimum: 8, allow_nil: true}
+  before_validation :ensure_session_token
+
+
+end
