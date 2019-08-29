413d7d70f10f11c6ae78f8655236f278fdccd962
kchhak <kane.chhak@gmail.com>
2019-08-29 04:38:35 -0700

    Creates class methods for User model

--- W9D4/Rails_auth/app/models/user.rb
+++ W9D4/Rails_auth/app/models/user.rb
@@ -4,5 +4,14 @@ class User < ApplicationRecord
   validates :password, length: {minimum: 8, allow_nil: true}
   before_validation :ensure_session_token
 
+  def self.find_by_credentials(username, password)
+    user = User.find_by(username: username)
+    return user if user && BCrypt::Password.new(user.password_digest).is_password?(password)
+  end
 
+  def self.generate_session_token
+    SecureRandom::urlsafe_base64(16)
+  end
+
+  
 end
